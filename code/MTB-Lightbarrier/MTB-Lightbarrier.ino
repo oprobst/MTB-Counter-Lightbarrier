9e1bb0d83a469185ed0eefdbfa8c52df52b3b78d
Oliver Probst <Oliverprobst@gmx.de>
2023-05-14 21:55:55 +0200

    Initial storing day counts

--- code/MTB-Lightbarrier/MTB-Lightbarrier.ino
+++ code/MTB-Lightbarrier/MTB-Lightbarrier.ino
@@ -71,10 +71,12 @@ long currentDayCycle = 1;
 
 long todayStartedAtMillis = 0;
 
-int todaysEEPROMlocation = 0;
+// EEPROM Location to store todays count.
+int todaysEepromByteAddress = 0;
+
 
 //count of contacts today
-short count = 0;
+int todayCount = 0;
 long overallCount; // = 32760;
 
 //indicates if the Light barrier is durable blocked (longer than 2 sec).
@@ -120,7 +122,6 @@ for (short i = 0; i < 4; i++) {
   overallCount = readOverallCount();
 
   determineTodayStorageByte();
-  //InitTimer1();
   watchdogSetup();
 }
  
@@ -177,6 +178,204 @@ void loop() {
   }
 }
 
+
+
+/* 
+ * Barrier should contacts longer 25ms (30cm object at 5km/h) and shorter 250ms (30cm object at 40km/h). 
+ * After first contact, we block the sensor for 300ms (two biker at 30km/h with a gap of 2,5m between).
+ * Return true, if at least 3 consecutive measurements are detecting a contact.
+ */
+bool checkBarrier() {
+  long startContact = millis();
+  bool detection = false;
+  permanentBlocked = false;
+  while (objectDetected()) {
+   // delay(1);
+    detection = true;
+    if (millis() > startContact + 2000) {
+      for (short i = 0; i < 10; i++) {
+        debugLED(ON);
+        delay(50);
+        debugLED(OFF);
+        delay(50);
+      }
+      permanentBlocked = true;
+      return false;
+    }
+  }
+  if (detection) {    
+    lastContactDuration = millis() - startContact;
+    storeOverallCount (++overallCount);
+    storeTodayCount(++todayCount);
+    delay(300);
+  }
+  return detection;
+}
+
+
+void showOutput() {
+  
+  lcd.clear();
+  lcd.noCursor();
+  lcd.setCursor(0, 0);  //col, row 
+  lcd.print(overallCount);
+  
+  lcd.setCursor(12, 0);  //col, row
+  lcd.print(readBatteryVoltage(), 1);
+  lcd.print("V");  
+
+  if (permanentBlocked){
+    lcd.setCursor(0, 1);  //col, row
+    lcd.print("LS BLOCKIERT    ");
+  }
+}
+
+void showDebugOutput() {
+  digitalWrite(PHOTODIODE_ON, HIGH);
+  delay(25);
+  int brightness = analogRead(PHOTODIODE);
+  digitalWrite(PHOTODIODE_ON, LOW);
+  int barrier = analogRead(LIGHTBARRIER);
+
+  lcd.clear();
+  lcd.noCursor();
+  lcd.setCursor(0, 0);  //col, row
+  lcd.print("Light:");
+  lcd.print(brightness);
+  if (checkForDaylight()) {
+    lcd.print("D");
+  } else {
+    lcd.print("N");
+  }
+
+  lcd.setCursor(12, 0);  //col, row
+  lcd.print(readBatteryVoltage(), 1);
+  lcd.print("V");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Today:");
+  lcd.print(todayCount);
+
+  lcd.setCursor(7, 1);
+  lcd.print(" Dur:");
+  lcd.print(lastContactDuration);
+}
+
+/* 
+ * True as long as the debug debug button is active.
+ */
+bool readDebugButton() {
+  return (digitalRead(DEBUG_BUTTON) == LOW);
+}
+
+/*
+ * True as long as the User debug button is active.
+ * Workaround for missing Pin6 digitalRead of arduino API (due to not used crystal)
+ */
+bool readUserButton() {
+  return !(PINB & (1 << PB6));
+}
+
+/*
+ * Turn debugLED on/off
+ */
+void debugLED(bool on) {
+  digitalWrite(DEBUG_LED, on);
+}
+/*
+ * Will check for brightness. 
+ * If the brightness is measured MEASUREMENT_LIMIT times higher than defined LIMIT_DAY, this method will returns always true. 
+ * If the brightness is measured MEASUREMENT_LIMIT times lower then defined LIMIT_NIGHT, this method will returns always false.
+ */
+bool checkForDaylight() {
+  digitalWrite(PHOTODIODE_ON, HIGH);
+  delay(50);
+  if (isDay) {
+    if (analogRead(PHOTODIODE) < LIMIT_NIGHT) {
+      brightnessLimit++;
+    }
+    if (brightnessLimit >= MEASUREMENTS_LIMIT) {
+      isDay = false;
+      brightnessLimit = 0;
+      debugLED(ON);
+      delay(200);
+      debugLED(OFF);
+      //storeLengthOfCurrentDay();
+    }
+  } else {
+    if (analogRead(PHOTODIODE) > LIMIT_DAY) {
+      isDay = true;
+      debugLED(ON);
+      delay(200);
+      debugLED(OFF);
+      todayStartedAtMillis = millis();  //set start of the day to now
+      determineTodayStorageByte();
+      todayCount = 0;
+    }
+  }
+  digitalWrite(PHOTODIODE_ON, LOW);
+  return isDay;
+}
+
+
+
+/*
+ * Turn display ON/OFF
+ */
+void activateDisplay(bool on) {
+  if (!on) {
+    digitalWrite(LCD_LED, LOW);
+    lcd.noDisplay();
+    displayOn = false;
+  } else {
+    digitalWrite(LCD_LED, HIGH);
+    lcd.display();
+    displayOn = true;
+    millisDisplayOff = millis() + DURATION_DISPLAY_ON;
+  }
+}
+
+/* 
+ *  Measure Battery voltage
+ */
+double readBatteryVoltage() {
+  return 14.3 / 1023.0 * (double)analogRead(BATTERY_VOLTAGE) + 0.7d;
+}
+
+
+/* 
+ *  returns true, when something blocks the light barrier
+ */
+bool objectDetected() {
+  return analogRead(LIGHTBARRIER) > 100;
+}
+
+
+/*
+ * Search for the first free EEPROM Address to store today's count later there.
+ */
+int determineTodayStorageByte() {
+  int todaysEepromByteAddress = 12;
+  short value = 1;
+  while (value != 0 && todaysEepromByteAddress++ < EEPROM.length() -1) {
+    value = EEPROM.read(todaysEepromByteAddress);
+  }  //found next empty entry
+  if (todaysEepromByteAddress >= EEPROM.length() -1) {
+    todaysEepromByteAddress = 12;
+  }
+  return todaysEepromByteAddress;
+}
+
+/*
+ * Store the count for today only
+ */
+void storeTodayCount (int value){
+  if (value > 255){
+    return;
+  }
+  EEPROM.update(todaysEepromByteAddress, value);
+  EEPROM.update(todaysEepromByteAddress + 0, 0);  
+}
 
 /*
  * Read the fist 3 long value from EEPROM. Check if at least 2 of 3 have the same value and return it. If they have completely different values, return the value of address 0.
@@ -237,196 +436,9 @@ void EEPROMWritelong(int address, long value) {
 }
 
 
-
-/* 
- * Barrier should contacts longer 25ms (30cm object at 5km/h) and shorter 250ms (30cm object at 40km/h). 
- * After first contact, we block the sensor for 300ms (two biker at 30km/h with a gap of 2,5m between).
- * Return true, if at least 3 consecutive measurements are detecting a contact.
- */
-bool checkBarrier() {
-  long startContact = millis();
-  bool detection = false;
-  permanentBlocked = false;
-  while (objectDetected()) {
-   // delay(1);
-    detection = true;
-    if (millis() > startContact + 2000) {
-      for (short i = 0; i < 10; i++) {
-        debugLED(ON);
-        delay(50);
-        debugLED(OFF);
-        delay(50);
-      }
-      permanentBlocked = true;
-      return false;
-    }
-  }
-  if (detection) {    
-    lastContactDuration = millis() - startContact;
-    storeOverallCount (++overallCount);
-    count++;
-    delay(300);
-  }
-  return detection;
-}
-
-
-void showOutput() {
-  
-  lcd.clear();
-  lcd.noCursor();
-  lcd.setCursor(0, 0);  //col, row 
-  lcd.print(overallCount);
-  
-  lcd.setCursor(12, 0);  //col, row
-  lcd.print(readBatteryVoltage(), 1);
-  lcd.print("V");  
-
-  if (permanentBlocked){
-    lcd.setCursor(0, 1);  //col, row
-    lcd.print("LS BLOCKIERT    ");
-  }
-}
-
-void showDebugOutput() {
-  digitalWrite(PHOTODIODE_ON, HIGH);
-  delay(25);
-  int brightness = analogRead(PHOTODIODE);
-  digitalWrite(PHOTODIODE_ON, LOW);
-  int barrier = analogRead(LIGHTBARRIER);
-
-  lcd.clear();
-  lcd.noCursor();
-  lcd.setCursor(0, 0);  //col, row
-  lcd.print("Light:");
-  lcd.print(brightness);
-  if (checkForDaylight()) {
-    lcd.print("D");
-  } else {
-    lcd.print("N");
-  }
-
-  lcd.setCursor(12, 0);  //col, row
-  lcd.print(readBatteryVoltage(), 1);
-  lcd.print("V");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Today:");
-  lcd.print(count);
-
-  lcd.setCursor(7, 1);
-  lcd.print(" Dur:");
-  lcd.print(lastContactDuration);
-}
-
-/* 
- * True as long as the debug debug button is active.
- */
-bool readDebugButton() {
-  return (digitalRead(DEBUG_BUTTON) == LOW);
-}
-
 /*
- * True as long as the User debug button is active.
- * Workaround for missing Pin6 digitalRead of arduino API (due to not used crystal)
+ * Watchdog interrupt every 8 sec, to wake up from power down mode at night.
  */
-bool readUserButton() {
-  return !(PINB & (1 << PB6));
-}
-
-/*
- * Turn debugLED on/off
- */
-void debugLED(bool on) {
-  digitalWrite(DEBUG_LED, on);
-}
-/*
- * Will check for brightness. 
- * If the brightness is measured MEASUREMENT_LIMIT times higher than defined LIMIT_DAY, this method will returns always true. 
- * If the brightness is measured MEASUREMENT_LIMIT times lower then defined LIMIT_NIGHT, this method will returns always false.
- */
-bool checkForDaylight() {
-  digitalWrite(PHOTODIODE_ON, HIGH);
-  delay(50);
-  if (isDay) {
-    if (analogRead(PHOTODIODE) < LIMIT_NIGHT) {
-      brightnessLimit++;
-    }
-    if (brightnessLimit >= MEASUREMENTS_LIMIT) {
-      isDay = false;
-      brightnessLimit = 0;
-      debugLED(ON);
-      delay(200);
-      debugLED(OFF);
-      //storeLengthOfCurrentDay();
-    }
-  } else {
-    if (analogRead(PHOTODIODE) > LIMIT_DAY) {
-      isDay = true;
-      debugLED(ON);
-      delay(200);
-      debugLED(OFF);
-      todayStartedAtMillis = millis();  //set start of the day to now
-    }
-  }
-  digitalWrite(PHOTODIODE_ON, LOW);
-  return isDay;
-}
-
-
-void determineTodayStorageByte() {
-  int address = 3;
-  short value = 1;
-  while (value != 0 && address++ < EEPROM.length()) {
-    value = EEPROM.read(address);
-  }  //found next empty entry
-  if (address >= EEPROM.length()) {
-    address = 3;
-  }
-  return address;
-}
-
-/*
-void storeLengthOfCurrentDay() {
-  long millisRun = millis() - todayStartedAtMillis;
-  short hours = millisRun / 3600000;
-
-  EEPROM.write(address, hours)
-}
-*/
-/*
- * Turn display ON/OFF
- */
-void activateDisplay(bool on) {
-  if (!on) {
-    digitalWrite(LCD_LED, LOW);
-    lcd.noDisplay();
-    displayOn = false;
-  } else {
-    digitalWrite(LCD_LED, HIGH);
-    lcd.display();
-    displayOn = true;
-    millisDisplayOff = millis() + DURATION_DISPLAY_ON;
-  }
-}
-
-/* 
- *  Measure Battery voltage
- */
-double readBatteryVoltage() {
-  return 14.3 / 1023.0 * (double)analogRead(BATTERY_VOLTAGE) + 0.7d;
-}
-
-
-/* 
- *  returns true, when something blocks the light barrier
- */
-bool objectDetected() {
-  return analogRead(LIGHTBARRIER) > 100;
-}
-
-
-
 void watchdogSetup(void) {
   cli();
   wdt_reset();
@@ -440,34 +452,3 @@ ISR(WDT_vect) {
 
 
 
-//************ timer
-//Timer1 Prescaler = 1024; Preload = 58593; Actual Interrupt Time = 60,000256 s
-
-
-
-
-//Place/Copy this part in declaration section
-void InitTimer1() {
-  cli();
-  TCCR1A = 0;
-  TCCR1B = 0;
-
-  /*2. We set the prescalar to the desired value by changing the CS10 CS12 and CS12 bits. */
-  TCCR1B |= B00000100;  //Set CS12 to 1 so we get prescalar 256
-
-  /*3. We enable compare match mode on register A*/
-  TIMSK1 |= B00000010;  //Set OCIE1A to 1 so we enable compare match A
-
-  /*4. Set the value of register A to 31250*/
-  OCR1A = 31250;  //Finally we set compare register A to this value
-  sei();
-}
-
-
-ISR(TIMER1_COMPA_vect) {
-  TCNT1 = 0;
-  //Enter your code here
-  debugLED(ON);
-  delay(40);
-  debugLED(OFF);
-}
